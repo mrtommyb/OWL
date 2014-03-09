a4e1e09b49b5767aa8b9fbb9c67a759a261ba757
davidwhogg <david.hogg@nyu.edu>
2014-03-09 15:36:38 +0000

    bug fixes, dumb logging

--- py/hlm.py
+++ py/hlm.py
@@ -12,15 +12,13 @@ Bugs:
 """
 
 import numpy as np
-import astropy.io.fits as pyfits
 import kplr
 client = kplr.API()
 
 # make shit for least squares
 # BUG: I am sure there is a one-liner for this
-XGRID, YGRID = np.meshgrid(np.arange(3) - 1., np.arange(3) - 1.)
+XGRID, YGRID = np.meshgrid(np.arange(3) - 1, np.arange(3) - 1)
 XGRID, YGRID = XGRID.flatten(), YGRID.flatten()
-print XGRID, YGRID
 NINEBYSIX = np.zeros((9,6))
 NINEBYSIX[:,0] = 1.
 NINEBYSIX[:,1] = XGRID
@@ -28,7 +26,6 @@ NINEBYSIX[:,2] = YGRID
 NINEBYSIX[:,3] = XGRID * XGRID
 NINEBYSIX[:,4] = XGRID * YGRID
 NINEBYSIX[:,5] = YGRID * YGRID
-print NINEBYSIX
 
 def get_target_pixel_file(kicid, quarter):
     kic = client.star(kicid)
@@ -38,7 +35,7 @@ def get_target_pixel_file(kicid, quarter):
         raise ValueError("No dataset for that quarter")
     return tpfs[0]
 
-def get_max_pixel(flux):
+def get_max_pixel(flux, mask):
     """
     input:
     * `flux` - `np.array` shape `(nt, ny, nx)` pixel intensities (not fluxes!)
@@ -50,12 +47,14 @@ def get_max_pixel(flux):
     * Asserts that the max pixel is not on an edge; might be dumb.
     """
     nt, ny, nx = flux.shape
-    max_indx = np.argmax(np.mean(flux, axis=0))
+    median_image = np.median(flux, axis=0)
+    median_image[(mask < 1)] = 0.
+    max_indx = np.argmax(median_image)
     xc, yc = max_indx % nx, max_indx / nx
-    assert (xc > 0)
-    assert (yc > 0)
-    assert ((xc + 1) < nx)
-    assert ((yc + 1) < ny)
+    if (xc < 1) or (yc < 1) or ((xc + 2) > nx) or ((yc + 2) > ny):
+        print "hlm.get_max_pixel(): ERROR: max pixel at image edge; failing!"
+        print "hlm.get_max_pixel():", mask, np.median(flux, axis=0), xc, yc
+        assert False
     return xc, yc
 
 def get_one_centroid(one_flux, invvar, xc, yc):
@@ -73,25 +72,23 @@ def get_one_centroid(one_flux, invvar, xc, yc):
 
     bugs:
     * Currently does NOTHING.
-    * Ought to do a solve operation, not an inverse operation.
     * Ought to do 9-fold leave-one-out set of operations for robustness.
     * Ought to take in a prior / regularization that draws the answer towards a default (floating point) centroid.
     """
     ninebyone = (one_flux[yc + YGRID, xc + XGRID]).flatten()
     iv = (invvar[yc + YGRID, xc + XGRID]).flatten()
     if np.sum(iv > 0.) < 7:
-        print "get_one_centroid(): not enough data to support fit"
-        print "get_one_centroid():", iv
-        print "get_one_centroid(): returning integer centroid"
+        print "hlm.get_one_centroid(): WARNING: not enough data to support fit"
+        print "hlm.get_one_centroid():", iv
+        print "hlm.get_one_centroid(): returning integer centroid"
         return xc, yc
     numerator = np.dot(np.transpose(NINEBYSIX), iv * ninebyone)
     denominator = np.dot(np.transpose(NINEBYSIX), iv[:, None] * NINEBYSIX)
-    print numerator.shape, denominator.shape
-    pars = np.dot(np.inverse(denominator), numerator)
+    pars = np.linalg.solve(denominator, numerator)
     if (pars[3] > 0.) or (pars[5] > 0.) or (pars[4] * pars[4] > pars[3] * pars[5]): # is this correct?
-        print "get_one_centroid(): fit bad"
-        print "get_one_centroid():", pars
-        print "get_one_centroid(): returning integer centroid"
+        print "hlm.get_one_centroid(): WARNING: fit bad"
+        print "hlm.get_one_centroid():", pars
+        print "hlm.get_one_centroid(): returning integer centroid"
         return xc, yc
     # now magic happens...
     return xc, yc
@@ -99,7 +96,8 @@ def get_one_centroid(one_flux, invvar, xc, yc):
 def get_all_centroids(flux, mask):
     """
     input:
-    * `flux` - pixel intensities (not fluxes!)
+    * `flux` - `np.array` shape `(nt, ny, nx)` of pixel intensities (not fluxes!)
+    * `mask` - Kepler-generated pixel mask.
 
     output:
     * `centroids` - some crazy object of centroids
@@ -108,8 +106,8 @@ def get_all_centroids(flux, mask):
     * Is `map()` dumb?
     * Should I be using a lambda function or something smarter?
     """
-    xc, yc = get_max_pixel(flux)
-    iv = zeros_like(flux[0])
+    xc, yc = get_max_pixel(flux, mask)
+    iv = np.zeros_like(flux[0])
     iv[(mask > 0)] = 1.
     def goc(c):
         return get_one_centroid(c, iv, xc, yc)
@@ -128,8 +126,4 @@ if __name__ == "__main__":
     time_in_kbjd = table["TIME"]
     # raw_cnts = table["RAW_CNTS"]
     bkg_sub_flux = table["FLUX"]
-    xc, yc = get_max_pixel(bkg_sub_flux)
     centroids = get_all_centroids(bkg_sub_flux, mask)
-    print np.median(bkg_sub_flux, axis=0)
-    print mask
-    print centroids.shape
