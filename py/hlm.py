32df87388ef7437d77517e4b29dc30ba53bff91c
davidwhogg <david.hogg@nyu.edu>
2014-03-15 15:49:29 -0400

    optimization in there but results make no sense

--- py/hlm.py
+++ py/hlm.py
@@ -66,17 +66,16 @@ def get_intensity_means_and_covariances(intensities, kplr_mask):
     * `mask` - what `kplr` calls `hdu[2].data` from the same
 
     outputs:
-    * `mean`
-    * `covar`
+    * `means` - one-d array of means for `kplr_mask > 0` pixels
+    * `covars` - two-d array of covariances for same
 
     bugs:
-    * Take medians NOT mean.
     * Uses for loops!
     * Needs more information in this comment header.
     """
     pixel_mask = get_pixel_mask(intensities, kplr_mask)
     epoch_mask = get_epoch_mask(pixel_mask, kplr_mask)
-    means = np.median(intensities[epoch_mask > 0, :, :], axis=0)
+    means = np.mean(intensities[epoch_mask > 0, :, :], axis=0)
     nt, ny, nx = intensities.shape
     covars = np.zeros((ny, nx, ny, nx))
     for ii in range(nx):
@@ -86,9 +85,12 @@ def get_intensity_means_and_covariances(intensities, kplr_mask):
                     if (kplr_mask[jj, ii] > 0) and (kplr_mask[ll, kk] > 0) and (covars[jj, ii, ll, kk] == 0):
                         mask = pixel_mask[:, jj, ii] * pixel_mask[:, ll, kk]
                         data = (intensities[:, jj, ii] - means[jj, ii]) * (intensities[:, ll, kk] - means[ll, kk])
-                        cc = np.median(data[(mask > 0)])
+                        cc = np.mean(data[(mask > 0)])
                         covars[jj, ii, ll, kk] = cc
                         covars[ll, kk, jj, ii] = cc
+    means = means[(kplr_mask > 0)]
+    covars = covars[(kplr_mask > 0)]
+    covars = covars[:, (kplr_mask > 0)]
     return means, covars
 
 def get_objective_function(weights, means, covars):
@@ -96,7 +98,8 @@ def get_objective_function(weights, means, covars):
     bugs:
     * Needs more information in this comment header.
     """
-    return 1.
+    wm = np.dot(weights, means)
+    return np.dot(weights, np.dot(covars, weights)) / (wm * wm)
 
 if __name__ == "__main__":
     kicid = 3335426
@@ -112,6 +115,13 @@ if __name__ == "__main__":
     # raw_cnts = table["RAW_CNTS"]
     intensities = table["FLUX"]
     means, covars = get_intensity_means_and_covariances(intensities, kplr_mask)
-    print covars[0, 0]
-    print covars[3, 2]
-    print covars[2, 3]
+    sap_weights = np.zeros(kplr_mask.shape)
+    sap_weights[kplr_mask == 3] = 1
+    sap_weights = sap_weights[kplr_mask > 0]
+    start_weights = np.ones(means.shape)
+    hlm_weights = op.fmin(get_objective_function, start_weights, args = (means, covars))
+    print "SAP", get_objective_function(sap_weights, means, covars)
+    print "start", get_objective_function(start_weights, means, covars)
+    print "HLM", get_objective_function(hlm_weights, means, covars)
+    print np.linalg.eig(covars)[0]
+    print hlm_weights
